97381b98308ab6c6e854a96dd12e0ef2872e9df8
Curtis Morte <curtismorte@gmail.com>
2013-03-17 17:46:52 -0700

    Update changed-javascript.js
    Added Notes

--- changed-javascript.js
+++ changed-javascript.js
@@ -6,11 +6,13 @@
         });
         setInterval(function () {
             if (b) {
+                /* Gathering the current URL and stripping the end / that goes before the #PostNumber */ 
             	var refc = window.location.pathname.slice(0,-1);
                 try {
                     var d = a(".post:in-viewport").first().attr("id"),
                         c = sessionStorage.getItem(d);
                     history.replaceState && c && (new_url = c + "/#" + d, history.replaceState("", "", new_url))
+                    /* Added Conditional for checking the URL to see if the page has changed */ 
                     if ((refc != c) && (c != '/page/1')) {
                         _gaq.push(['_trackPageview', c]);
                     }
